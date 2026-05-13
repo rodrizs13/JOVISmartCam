b3aa3d533c9035d08dd724bdc9def1903a384575
rodrizs13 <lucasrodriguessantos569@gmail.com>
2026-05-13 20:35:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 
+// ALERT + PROMPT
+let nome = prompt("Digite seu nome:");
+alert("Bem-vindo, " + nome + "!");
+
+// LOGIN
+function login() {
+    let u = usuario.value;
+    let s = senha.value;
+
+    if(u === "" || s === "") {
+        alert("Preencha tudo!");
+    } else {
+        status.innerText = "Bem-vindo, " + u + "!";
+    }
+}
+
+// BOTÃO
+function clicar() {
+    resposta.innerText = "A IA foi ativada!";
+}
+
+// SLIDESHOW + TEXTO
+let imagens = ["cachhoro.png", "grava.png", "nova.png"];
+let textos = [
+    "Fotos ilegíveis e dificuldade ao registrar conteúdos.",
+    "Caderno digital nítido e ganho de tempo.",
+    "IA escolhe automaticamente a melhor imagem."
+];
+
+let i = 0;
+
+function proxima() {
+    i = (i + 1) % imagens.length;
+    slide.src = imagens[i];
+    textoSlide.innerText = textos[i];
+}
+
+// EVENTO + INICIAL
+document.addEventListener("DOMContentLoaded", () => {
+    textoSlide.innerText = textos[0];
+
+    resposta.onmouseover = () => resposta.style.color = "yellow";
+});
